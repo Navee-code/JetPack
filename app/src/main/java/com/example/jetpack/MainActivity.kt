a6fe924a8fcbfc28cdbe868614623f4a89aa1760
Naveen <balaji@brochill.com>
2023-06-16 00:11:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/jetpack/MainActivity.kt
@@ -0,0 +1,107 @@
+package com.example.jetpack
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpack.ui.theme.JetPackTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetPackTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                    EditableTextField()
+                    ButtonField()
+                }
+            }
+        }
+    }
+}
+@Composable
+fun ButtonField(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+       Button(onClick = {  }) {
+
+           
+       }
+
+    }
+}
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditableTextField( modifier: Modifier = Modifier) {
+    var text by remember { mutableStateOf(TextFieldValue()) }
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.TopCenter.also {
+
+        }
+    ) {
+        TextField(
+            value = text,
+            onValueChange = { newValue -> text = newValue },
+            label = { Text(text = "Enter your name") },
+            modifier = Modifier
+                .width(200.dp)
+                .height(50.dp)
+
+        )
+    }
+
+}
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetPackTheme {
+        Greeting("Android")
+        EditableTextField()
+        ButtonField()
+    }
+}
